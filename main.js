b078afe8838b105c8d661697a31a50b2b6b524ad
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-28 17:18:52 -0300

    rodando o fetch com o método POST para adicionar usuários na API através de um JSON e imrpimindo os mesmos na tela

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//const { json } = require("stream/consumers");
+
 const url = "http://localhost:5500/api";
 
 function getUsers() {
@@ -16,5 +18,28 @@ function getUser() {
     .catch(error => console.log(error))
 }
 
+function addUser(newUser) {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        
+            
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertAPI.textContent = data)
+    .catch(error => console.log(error))
+} 
+
+
+const newUser = {
+    name: "Reinaldo Ramos",
+    avatar: "https://i.pinimg.com/originals/bf/f6/74/bff674de999d4313915189b073babadd.jpg",
+    city: "Rio do Sul"
+}
+
+addUser(newUser)
 getUser();
 getUsers();
